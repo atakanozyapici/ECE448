816ca4023fc608e644885bccb4d5b882c0422d23
Atakan Ozyapici <atakanozyapici@gmail.com>
2020-08-31 16:27:25 -0500

    first commit

---
+++ mp1-code/search.py
@@ -0,0 +1,118 @@
+# search.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Michael Abir (abir2@illinois.edu) on 08/28/2018
+
+"""
+This is the main entry point for MP1. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+# Search should return the path.
+# The path should be a list of tuples in the form (row, col) that correspond
+# to the positions of the path taken by your search algorithm.
+# maze is a Maze object based on the maze from the file specified by input filename
+# searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi,fast)
+
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+def search(maze, searchMethod):
+    return {
+        "bfs": bfs,
+        "astar": astar,
+        "astar_corner": astar_corner,
+        "astar_multi": astar_multi,
+        "fast": fast,
+    }.get(searchMethod)(maze)
+
+def bfs(maze):
+    """
+    Runs BFS for part 1 of the assignment.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    # TODO: Write your code here
+    visited = {} #dictionary
+    q = queue.Queue()
+    start = maze.getStart()
+    q.put(start)
+    visited[start] = start
+
+    while(q.empty() == False):
+        current = q.get()
+        neighbors = maze.getNeighbors(current[0], current[1])
+        for i in neighbors:
+            b_flag = 0
+            if(maze.isObjective(i[0], i[1]) == True):
+                visited[i] = current
+                current = i
+                b_flag = 1
+                break
+            if(i not in visited):
+                q.put(i)
+                visited[i] = current
+        if(b_flag):
+            break
+
+    ret = []
+    while (current != start):
+        ret.insert(0,current)
+        current = visited[current]
+    ret.insert(0,start)
+    return ret
+
+
+def astar(maze):
+    """
+    Runs A star for part 1 of the assignment.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    # TODO: Write your code here
+    return []
+
+def astar_corner(maze):
+    """
+    Runs A star for part 2 of the assignment in the case where there are four corner objectives.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+        """
+    # TODO: Write your code here
+    return []
+
+def astar_multi(maze):
+    """
+    Runs A star for part 3 of the assignment in the case where there are
+    multiple objectives.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    # TODO: Write your code here
+    return []
+
+
+def fast(maze):
+    """
+    Runs suboptimal search algorithm for part 4.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    # TODO: Write your code here
+    return []
